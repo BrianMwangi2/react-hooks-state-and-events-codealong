5945affdbf6c93ff96783608552e0c8f71466168
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-30 12:48:16 +0300

    adds files

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+/*function Toggle() {
+ let [on, setIsOn] = useState(false);
+ function handleClick() {
+    setIsOn((isOn)=> !isOn);
+  }
+  const color=isOn ? "red" : "white";
+  return <button style={{background : color}}onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+// Here we are using the useState hook to create a state variable called on and a function called setIsOn.
+// we're doing some conditional rendering to dynamically determine button's text based on the value of the on state variable.
 }
 
+
+export default Toggle;
+*/
+function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
+}
 export default Toggle;
